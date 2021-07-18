cd69204d0c2e697fd05663c798b1c4b93d735f86
Lord-Nthiga <travis.nthiga@student.moringaschool.com>
2021-07-19 00:52:16 +0300

    addes assets folder

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var btn1 = ("male");
-var btn2 = ("female");
+var btn1 = ("Male");
+var btn2 = ("Female");
 var days ="Sunday, Monday, Tuesday, Wednesday,Thursday, Friday, Saturday";
 var boysName = "Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame";
 var girlsName = "Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama";
+if  (btn1 === Male)  {}
+if  (btn2 === Female)  {}
