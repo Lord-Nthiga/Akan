16fd6c789d5b612d7eb1d857e332f6d7b2cf5e9a
Lord-Nthiga <travis.nthiga@student.moringaschool.com>
2021-07-19 20:07:39 +0300

    polishing project

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var name = (yy,mm,d,)=> {
-    var days =["Sunday, Monday, Tuesday, Wednesday,Thursday, Friday, Saturday";]
+
+    var days =["Sunday, Monday, Tuesday, Wednesday,Thursday, Friday, Saturday"];
     var boysName = ["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"];
     var girlsName = ["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"];
-        
+    var submit = document.querySelector("#buttonsubmit");
+    submit .addEventListener("click", function() {
+        var gender = 
+        document .querySelector('input[name="gender"]:checked');
+        if  (genser === null) {
+            prompt("Kindly Select your Gender");
+        }
+        else {console.log(gender.value)};
+    })
+     
